ea1a12f85cbdcc9f81f929b52b234a93e4ffc895
didinem <didinem@sina.com>
2017-03-09 22:12:03 +0800

    init

--- src/main/java/org/didinem/listener/RegistryListener.java
+++ src/main/java/org/didinem/listener/RegistryListener.java
@@ -1,17 +1,12 @@
 package org.didinem.listener;
 
-import com.alibaba.dubbo.common.URL;
-import com.google.common.cache.LoadingCache;
-import org.I0Itec.zkclient.IZkChildListener;
 import org.I0Itec.zkclient.ZkClient;
-import org.apache.commons.lang3.StringUtils;
 import org.didinem.sprops.DubboProperties;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by zhongzhengmin on 2017/3/8.
@@ -22,37 +17,39 @@ public class RegistryListener implements InitializingBean {
     @Autowired
     private DubboProperties dubboProperties;
 
+//    @Autowired
+//    private LoadingCache<String, String> loadingCache;
+
     @Autowired
-    private LoadingCache<String, String> loadingCache;
+    private ZkClient zkClient;
 
     public RegistryListener() {
     }
 
-    private void init() {
-        String serverString = dubboProperties.getAddress();
-        ZkClient zkClient = new ZkClient(serverString);
-        String dubboRootPath = "/dubbo";
+    private void initListener() {
+        String seperator = dubboProperties.getSeperator();
+        String dubboRootPath = seperator + dubboProperties.getRoot();
         List<String> pathList = zkClient.getChildren(dubboRootPath);
         for (String path : pathList) {
             String providerPath = dubboRootPath + "/" + path + "/providers";
             System.out.println("start to listen:" + providerPath);
-            zkClient.subscribeChildChanges(providerPath, new IZkChildListener() {
-                public void handleChildChange(String s, List<String> list) throws Exception {
-                    System.out.println(s);
-                    System.out.println("=================================================");
-                    for (String string : list) {
-                        System.out.println("url is " + string);
-                        if (StringUtils.isNotEmpty(string)) {
-                            System.out.println("url is " + string);
-                            URL url = URL.valueOf(URL.decode(string));
-                            Map<String, String> map = url.toMap();
-                            for (String key : map.keySet()) {
-                                System.out.println(String.format("Key is %s and value is %s", key, map.get(key)));
-                            }
-                        }
-                    }
-                }
-            });
+//            zkClient.subscribeChildChanges(providerPath, new IZkChildListener() {
+//                public void handleChildChange(String s, List<String> list) throws Exception {
+//                    System.out.println(s);
+//                    System.out.println("=================================================");
+//                    for (String string : list) {
+//                        System.out.println("url is " + string);
+//                        if (StringUtils.isNotEmpty(string)) {
+//                            System.out.println("url is " + string);
+//                            URL url = URL.valueOf(URL.decode(string));
+//                            Map<String, String> map = url.toMap();
+//                            for (String key : map.keySet()) {
+//                                System.out.println(String.format("Key is %s and value is %s", key, map.get(key)));
+//                            }
+//                        }
+//                    }
+//                }
+//            });
         }
 
 
@@ -61,6 +58,6 @@ public class RegistryListener implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        init();
+        initListener();
     }
 }
--- src/main/java/org/didinem/sconf/ZooKeeperConfiguration.java
+++ src/main/java/org/didinem/sconf/ZooKeeperConfiguration.java
@@ -1,7 +1,26 @@
 package org.didinem.sconf;
 
+import org.I0Itec.zkclient.ZkClient;
+import org.didinem.sprops.DubboProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * Created by didinem on 3/9/2017.
  */
+@Configuration
 public class ZooKeeperConfiguration {
+
+    @Autowired
+    private DubboProperties dubboProperties;
+
+    @Bean("zkClient")
+    public ZkClient getZkClient() {
+        String serverString = dubboProperties.getAddress();
+        ZkClient zkClient = new ZkClient(serverString);
+        return zkClient;
+    }
+
+
 }
